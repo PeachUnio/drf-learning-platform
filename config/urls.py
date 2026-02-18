3ff63dc90b9069fed10d87adc66354523878b2c9
Арина Новгородова <arrrishanov@gmail.com>
2026-02-18 20:08:07 +0300

    add url for materials app

--- config/urls.py
+++ config/urls.py
@@ -5,6 +5,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    # path("", include("materials.urls", namespace="materials")),
+    path("materials/", include("materials.urls", namespace="materials")),
     # path("user/", include("users.urls", namespace="users")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
