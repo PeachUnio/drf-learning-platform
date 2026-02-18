d8bc80f94eca65eca45b31508ef2ca9d59b807ee
Арина Новгородова <arrrishanov@gmail.com>
2026-02-18 20:08:39 +0300

    add view for Lessons

--- materials/views.py
+++ materials/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from materials.models import Lesson
+from materials.serializer import LessonSerializer
+
+
+class LessonViewSet(ModelViewSet):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
