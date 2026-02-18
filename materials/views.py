a5c40102efac4962b438d36317e5ef82c2299018
Арина Новгородова <arrrishanov@gmail.com>
2026-02-18 20:42:23 +0300

    add views for Course

--- materials/views.py
+++ materials/views.py
@@ -1,9 +1,35 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
 
-from materials.models import Lesson
-from materials.serializer import LessonSerializer
+from materials.models import Lesson, Course
+from materials.serializer import LessonSerializer, CourseSerializer
 
 
 class LessonViewSet(ModelViewSet):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+
+
+class CourseCreateAPIView(CreateAPIView):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+
+class CourseListAPIView(ListAPIView):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+
+class CourseRetrieveAPIView(RetrieveAPIView):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+
+class CourseUpdateAPIView(UpdateAPIView):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+
+class CourseDestroyAPIView(DestroyAPIView):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
