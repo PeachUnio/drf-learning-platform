e6aa005f434a9d32a9699365b046d38edc55c075
Арина Новгородова <arrrishanov@gmail.com>
2026-02-18 20:09:06 +0300

    add url for Lessons

---
+++ materials/urls.py
@@ -0,0 +1,11 @@
+from rest_framework.routers import SimpleRouter
+
+from materials.apps import MaterialsConfig
+from materials.views import LessonViewSet
+
+app_name = MaterialsConfig.name
+router = SimpleRouter()
+router.register("", LessonViewSet)
+
+urlpatterns = []
+urlpatterns += router.urls
