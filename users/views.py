85cbe89a6fd23ac07da1287b9b229c1547a33a0e
Арина Новгородова <arrrishanov@gmail.com>
2026-02-19 13:28:01 +0300

    add view for user model

--- users/views.py
+++ users/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.generics import RetrieveUpdateAPIView
 
-# Create your views here.
+from users.models import User
+from users.serializer import UserSerializer
+
+
+class UserProfileView(RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        return self.request.user
