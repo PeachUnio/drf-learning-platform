6c6cc51289a976acb31d22babee7720060c67bd9
Арина Новгородова <arrrishanov@gmail.com>
2026-02-22 12:33:44 +0300

    add url for payment

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
 from users.apps import UsersConfig
-from users.views import UserProfileView
+from users.views import UserProfileView, PaymentListAPIView
 
 
 app_name = UsersConfig.name
 
 urlpatterns = [
-    path('profile/', UserProfileView.as_view(), name='user_profile'),
+    path("profile/", UserProfileView.as_view(), name="user_profile"),
+    # платежи
+    path("payments/", PaymentListAPIView.as_view(), name="payment_list")
 ]
