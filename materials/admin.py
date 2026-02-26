a44a44b984b09760cd8a6f12f108241ab6787d60
Арина Новгородова <arrrishanov@gmail.com>
2026-02-26 21:19:23 +0300

    register models in admin

--- materials/admin.py
+++ materials/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from materials.models import Lesson, Course
 
-# Register your models here.
+@admin.register(Lesson)
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "course", "auth")
+    list_filter = ("course", "auth")
+    search_fields = ("name", "description")
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "auth")
